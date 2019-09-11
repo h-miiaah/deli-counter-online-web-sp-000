e5f4a1ea0f40b2494298cf4f0d2edac8b792957e
Haim D <haim11293@aol.com>
2019-09-11 12:04:22 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,35 @@
 # Write your code here.
+
+#creates an emoty array for the deli line.
+katz_deli = []
+
+# takes the line, checks if it empty and print message, if not empty prints current position and customer name.
+def line(katz_deli)
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    current_line = "The line is currently:"
+    katz_deli.each.with_index(1) do |customer, position|
+      current_line << " #{position}. #{customer}"
+    end
+  end
+  puts current_line
+end
+
+
+# greets and assigns customer a position in line.
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+
+# if line is empty prints message, if not prints who is next in line and removes first customer from the line array.
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+  end
+end
